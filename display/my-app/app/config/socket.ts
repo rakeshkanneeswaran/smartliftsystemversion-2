50bf62b192afe4585154e4021875d3c7f507c68e
rakeshkanneeswaran <rakikanneeswaran@gmail.com>
2026-01-08 22:42:12 +0530

    update backend

--- display/my-app/app/config/socket.ts
+++ display/my-app/app/config/socket.ts
@@ -1,8 +1,6 @@
 import { io } from "socket.io-client";
 
-const BACKEND_URL =
-    process.env.NEXT_PUBLIC_BACKEND_URL ||
-    `ws://${typeof window !== "undefined" ? window.location.hostname : "localhost"}:3002`;
+
 
 export const socket = io(
     process.env.NEXT_PUBLIC_BACKEND_URL ??
